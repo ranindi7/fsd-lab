832dd512cd495f73bfcba1021e06513450405e00
Ranindi Gunasekera <ngunasekera3@academic.rrc.ca>
2025-11-09 00:00:37 -0600

    Added form component in Employee page to handle adding new employee to list

--- fsd-lab-01/src/components/pages/employeeData.tsx
+++ fsd-lab-01/src/components/pages/employeeData.tsx
@@ -1,13 +1,43 @@
+import { useState } from "react";
 import employeeData from "../../jsonData/employees.json";
 import type { Employee } from "../../types/employeeInterface";
 
 export default function EmployeeData() {
+    const[employeeInfo, setEmployeeInfo] = useState<Employee[]>(employeeData)
+
+    const[newEmployeeName, setNewEmployeeName] = useState("");
+    const[selectedDept, setSelectedDept] = useState("");
+    const[error, setError] = useState("");
+
+    const handleAddEmployee = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (newEmployeeName.trim().length < 3) {
+            setError("Name must have at least 3 characters.");
+            return;
+        }
+
+        const updatedData = employeeInfo.map((dept) => {
+            if (dept.department === selectedDept) {
+                return {
+                    ...dept,
+                    employees: [...dept.employees, newEmployeeName.trim()]
+                }
+            } 
+            return dept;
+        });
+
+        setEmployeeInfo(updatedData);
+        setNewEmployeeName("");
+        setError("");
+    }
+
     return(
         <main>
             <h1>Employee Directory</h1>
 
             <div>
-                {employeeData.map((emps: Employee) => (
+                {employeeInfo.map((emps: Employee) => (
                 <div className="employeesDisplay">
                     <h2>{emps.department}</h2>
                     <ul>
@@ -17,6 +47,42 @@ export default function EmployeeData() {
                     </ul>
                 </div>
                 ))}
+            </div>
+
+            <div>
+                <form onSubmit={handleAddEmployee}>
+                    <h3>Add New Employee</h3>
+
+                    <div>
+                        <label>Name: </label>
+                        <input
+                            type="text"
+                            value={newEmployeeName}
+                            onChange={(e) => {
+                                setNewEmployeeName(e.target.value);
+                                if (e.target.value.trim().length >= 3) setError("")
+                            }}
+                        />   
+                        {error && <p style={{ color: "red" }}>{error}</p>}
+                    </div>
+
+
+                    <div>
+                    <label>Department: </label>
+                    <select
+                        value={selectedDept}
+                        onChange={(e) => setSelectedDept(e.target.value)}
+                    >
+                        {employeeData.map((dept) => (
+                        <option key={dept.department} value={dept.department}>
+                            {dept.department}
+                        </option>
+                        ))}
+                    </select>
+                    </div>
+
+                    <button type="submit">Add Employee</button>
+                </form>
             </div>
         </main>
     )
--- fsd-lab-01/src/components/pages/organizationData.tsx
+++ fsd-lab-01/src/components/pages/organizationData.tsx
@@ -5,7 +5,8 @@ export function OrganizationData({
   rolesList
 }: {
   rolesList: Organization[];
-}) {
+}) 
+{
     return(
         <>
         <main>
